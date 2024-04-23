f262eb2dca45c9004d58ed5c8cdf5d8cb3cdb7e1
jschaefe-akw <janosch.schaefer@karcher.com>
2024-04-23 15:15:39 +0200

    add ecr permissions

--- modules/ssvc_ci_role_eks_config/main.tf
+++ modules/ssvc_ci_role_eks_config/main.tf
@@ -43,6 +43,10 @@ data "aws_iam_policy_document" "this" {
   statement {
     effect = "Allow"
     actions = [
+      "ecr:GetRepositoryPolicy",
+      "ecr:GetLifecyclePolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListTagsForResource",
       "eks:DescribeCluster",
     ]
     resources = ["*"]
